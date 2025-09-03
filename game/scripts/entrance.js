eaa79e8d5cdba61f4ee79db254581afdc60cd355
Alex Hebert <alex.p.hebert.dev@gmail.com>
2025-09-02 21:25:46 -0500

    FIX case sensitivity in entrance

--- game/scripts/entrance.js
+++ game/scripts/entrance.js
@@ -1,7 +1,7 @@
 function checkPassword() {
     const correctPassword = "friend";
     const password = document.getElementById("password").value;
-    if (password === correctPassword) {
+    if (password.toLowerCase() === correctPassword) {
         window.location.href = "watcher.html";
     } else {
         alert("The door stands firm and silent, not responding to your words. Try again.");
